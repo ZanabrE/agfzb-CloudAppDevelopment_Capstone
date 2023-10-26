48126dcfe56dbc3a339274063502dbb0991259b6
ZanabrE <ernesto.zanabria1979@gmail.com>
2023-10-26 19:43:45 -0400

    modified: djangoapp/urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,10 +11,11 @@ urlpatterns = [
     #path(route='', view=views.sample, name='sample'),
     
     # path for about view
-    #path(route='', view=views.about, name='about'),
+    path(route='about', view=views.about, name='about'),
 
     # path for contact us view
-
+    path(route='contact', view=views.contact, name='contact'),
+    
     # path for registration
 
     # path for login
