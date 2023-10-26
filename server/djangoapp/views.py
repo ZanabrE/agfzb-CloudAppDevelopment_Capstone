efff020a509903719c76ac9583aba3a4bf79b72e
ZanabrE <ernesto.zanabria1979@gmail.com>
2023-10-25 21:25:08 -0400

    djangoapp/views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -22,7 +22,9 @@ logger = logging.getLogger(__name__)
 
 # Create an `about` view to render a static about page
 def about(request):
-    return render(request, "about.html")
+    context = {}
+    if request.method == "GET":
+    return render(request, 'djangoapp/about.html', context)
 
 
 # Create a `contact` view to return a static contact page
