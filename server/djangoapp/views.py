3acedb2f92479e60fdc06ad0c3c078e09417b740
ZanabrE <ernesto.zanabria1979@gmail.com>
2023-12-24 13:39:10 -0500

    modified: djangoapp/views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -47,7 +47,7 @@ def login_request(request):
 
     if request.method == "POST":
         username = request.POST['username']
-        password = request.POST['psw']
+        password = request.POST['password']
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
