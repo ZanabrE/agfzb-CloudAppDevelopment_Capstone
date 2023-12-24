644b0b18e7f002c63b9ec4ed6225168ab0a149a4
ZanabrE <ernesto.zanabria1979@gmail.com>
2023-12-24 12:56:59 -0500

    modified: djangoapp/views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -101,7 +101,7 @@ def registration_request(request):
 def get_dealerships(request):
     if request.method == "GET":
         context = {}
-        url = "https://ernestozanab-3000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
+        url = "https://ernestozanab-3000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
         dealerships = get_dealers_from_cf(url)
         context["dealership_list"] = dealerships
         return render(request, 'djangoapp/index.html', context)
